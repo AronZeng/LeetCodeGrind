bef1a16bb3c6778dc291182b853701a3c98e0716
Aron Zeng <aron@Arons-MacBook-Pro.local>
2019-09-04 23:31:11 -0400

    Save Changes

--- twosum.py
+++ twosum.py
@@ -1,12 +1,12 @@
 
-    def twoSum ( array , target):
-        currentIndex = 0
-        for i in array[ : (len(array)) - 1 ]:
-            if i < target:
-                remainder = target - i
-                if remainder in array[currentIndex + 1 : ] : 
-                   remainderIndex = array[currentIndex + 1 : ].index(remainder) + currentIndex + 1
-                   return([currentIndex , remainderIndex])
-                else: 
-                    currentIndex += 1    
-        return[] 
+def twoSum ( array , target):
+    currentIndex = 0
+    for i in array[ : (len(array)) - 1 ]:
+        if i < target:
+            remainder = target - i
+            if remainder in array[currentIndex + 1 : ] : 
+                remainderIndex = array[currentIndex + 1 : ].index(remainder) + currentIndex + 1
+                return([currentIndex , remainderIndex])
+            else: 
+                currentIndex += 1    
+    return[] 
