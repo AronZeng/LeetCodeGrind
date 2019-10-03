02c1cac11cdcb787100c4e61368f1911d7caaaee
Aron Zeng <aron.1998.zeng@gmail.com>
2019-10-03 17:22:34 -0400

    Revert "Few More Problems"
    This reverts commit 9fd69c0d7bfdc6443368e12d099e2246531489f6.

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -7,7 +7,8 @@ class Solution(object):
     def addTwoNumbers(self , l1 , l2 , carry = 0):
         sum = l1.val + l2.val + carry
         carry = sum // 10
-        ret = ListNode(sum % 10)     
+        ret = ListNode(sum % 10)
+        ret = ListNode(sum % 10)        
         if l1.next != None or l2.next != None:
             if l1.next == None:
                 l1.next = ListNode(0)
--- LongestSubstring.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        checker = false
-        index = 0
-        substring = ""
-        while checker == false: 
-            
